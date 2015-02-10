c6d59fe19b5583e6522f8ef379102362aa610c02
Pietdagamer <Pietdagamer@users.noreply.github.com>
2015-02-10 22:26:10 +0100

    a

---
+++ Result.java
@@ -0,0 +1,23 @@
+package mastermind;
+
+public class Result 
+{
+	private char[] results;
+	
+	public Result(char[] results)
+	{
+		this.results = results;
+	}
+
+	public char[] getResults() 
+	{
+		return results;
+	}
+
+	public void setResults(char[] results) 
+	{
+		this.results = results;
+	}
+	
+	
+}
